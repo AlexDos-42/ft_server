66f10eca258122e491c9ce80a490a8cac0209fd7
Alex SANTOS <alesanto@e2r6p18.42.fr>
2020-02-09 15:40:39 +0100

    ft_serveur termine

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,23 @@
-#sys exploitation
+# sys exploitation
 FROM debian:buster
 MAINTAINER Alex Dos Santos <alesantostudent.42.fr>
 
-#y = oui && q = ?
+# y = oui && q = silence
 RUN apt-get -y update -q \
 	&& apt-get -y upgrade -q \
-#wget = programme de telechargement
+# wget = programme de telechargement
 	&& apt-get -y install wget -q \
-#nginx = serveur Web 	
+# nginx = serveur Web 	
 	&& apt-get -y install nginx -q \
-#mariaDB = système de gestion de base de données
+# mariaDB = système de gestion de base de données
 	&& apt-get -y install mariadb-server -q \
+# langage de scripts généraliste
 	&& apt-get -y install php7.3-fpm php7.3-mysql -q
 
-ADD 
+COPY ./srcs/* srcs_docker/ 
+
+RUN chmod +x /srcs_docker/docker.sh
+
+CMD bash /srcs_docker/docker.sh
 
 EXPOSE 80 443
-
-ENTRYPOINT []
--- srcs/docker.sh
+++ srcs/docker.sh
@@ -4,6 +4,35 @@ service nginx start
 service mysql start
 service php7.3-fpm start
 
-#########  install wordpress
-wget -c https://wordpress.org/latest.tar.gz -O - | tar -xz -C /var/www
-chown -R www-data: /var/www/wordpress      #allow nginx complete access
+#tar
+#-xz extrait sans remplacer les fichiers existants
+
+#-O - | ES
+
+#wordpress
+wget -c  https://wordpress.org/latest.tar.gz -O - | tar -xz -C /var/www
+chown -R www-data: /var/www/wordpress      
+
+#phpmyadmin
+wget -c https://files.phpmyadmin.net/phpMyAdmin/4.9.2/phpMyAdmin-4.9.2-english.tar.gz -O - | tar -xz -C /var/www
+mv var/www/phpMyAdmin-4.9.2-english var/www/phpmyadmin
+chown -R www-data: /var/www/phpadmin 
+
+#nginx for wordpress and phpmyadmin
+cp srcs_docker/nginx-block /etc/nginx/sites-available/nginx-block
+ln -s /etc/nginx/sites-available/nginx-block  /etc/nginx/sites-enabled/nginx-block
+rm /etc/nginx/sites-enabled/default
+
+mysql < srcs_docker/db.sql
+
+#ssl
+#req -x509 self signed
+#	 -nodes	not encryptded
+#	 -days number of days to certifythe certificate with -x509
+#	 -newkey new certificate request and key rsa:nbits
+#	 -keyout the filename to write
+#	 -out output filename to write
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt < srcs_docker/certificate
+
+service nginx restart
+/bin/sh
